b000cdf25a0905f107b9992142c06e5be5e5b562
wiktor-jedski <jedrzejewskiwiktor@gmail.com>
2022-12-15 12:53:56 +0100

    Changed the way new analysis is added and shown - added a model, changed the format of analysis_list.html and base.html

--- data/models.py
+++ data/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Analysis(models.Model):
+    """
+    Defines an Analysis that can be showed on the website.
+    """
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    link = models.CharField(max_length=100)
+    photo = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
--- showcase/settings.py
+++ showcase/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = ''
-STATICFILES_DIRS = ('static',)
+STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS = ('static',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
